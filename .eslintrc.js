9020422c6e0c78ba14e564e6c2264176f010527d
胡雄柏 <1556511170@qq.com>
2023-08-17 17:45:10 +0800

    chore: 修改代码格式化的配置，并添加格式化的命令

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,28 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 要求使用 let 或 const 而不是 var
+    "no-var": "error",
+    // 要求使用 const 声明那些声明后不再被修改的变量
+    "prefer-const": "error",
+    // 声明但未使用的变量警告
+    "no-unused-vars": "warn",
+    // 强制switch要有default分支
+    "default-case": 2,
+    // 强制使用一致的缩进（"SwitchCase"（默认：0) 强制 switch 语句中 case 子句的缩进级别。缩进 2 个空格并将 SwitchCase 设置为 1 将缩进 case 子句，其中 2 个空格相对于 switch 语句。）
+    indent: 0,
+    // 强制使用骆驼拼写法命名约定
+    camelcase: 0,
+    // 函数表达式必须有名字
+    "func-names": 0,
+    // 单引号
+    "single-quote": 0,
   },
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -8,19 +8,19 @@ module.exports = {
   // 行尾有分号
   semi: true,
   // 使用单引号代替双引号
-  singleQuote: true,
+  singleQuote: false,
   // 对象的 key 仅在必要时用引号
-  quoteProps: 'as-needed',
+  quoteProps: "consistent",
   // jsx 不使用单引号，而使用双引号
   jsxSingleQuote: false,
   // 末尾使用逗号
-  trailingComma: 'all',
+  trailingComma: "all",
   // 大括号内的首尾需要空格 { foo: bar }
   bracketSpacing: true,
   // jsx 标签的反尖括号需要换行
   jsxBracketSameLine: false,
   // 箭头函数，只有一个参数的时候，也需要括号
-  arrowParens: 'always',
+  arrowParens: "always",
   // 每个文件格式化的范围是文件的全部内容
   rangeStart: 0,
   rangeEnd: Infinity,
@@ -29,9 +29,11 @@ module.exports = {
   // 不需要自动在文件开头插入 @prettier
   insertPragma: false,
   // 使用默认的折行标准
-  proseWrap: 'preserve',
-  // 指定HTML对空格的敏感度，以处理空格
-  htmlWhitespaceSensitivity: 'css',
+  proseWrap: "preserve",
+  // 指定HTML对空格的敏感度。所有空格被视为不敏感，皆采用「空格不敏感格式化」，会去除空格。
+  htmlWhitespaceSensitivity: "ignore",
   // 换行符。可选"auto"、"lf"、"cr"、"crlf"
-  endOfLine: 'auto',
+  endOfLine: "lf",
+  // 用于 Markdown 等代码的格式化，自动语言识别
+  embeddedLanguageFormatting: "auto",
 };
